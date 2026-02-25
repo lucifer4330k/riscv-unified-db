f12f77c3fc0ded3b6f9aa8faf4803638fefacbca
Debanjan Maji <debanjanmaji700@gmail.com>
2026-02-26 03:04:43 +0530

    style: reorder test header comments to match convention

--- tools/ruby-gems/udb/test/test_instruction.rb
+++ tools/ruby-gems/udb/test/test_instruction.rb
@@ -1,6 +1,6 @@
-# typed: false
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
+# typed: false
 
 # frozen_string_literal: true
 
